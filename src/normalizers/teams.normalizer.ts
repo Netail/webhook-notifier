2a24a15acd3c80a54ff9697ed1c57229545c5ee5
Maikel <maikel.van.dort@gmail.com>
2024-08-26 15:20:06 +0200

    Update teams.normalizer.ts

--- src/normalizers/teams.normalizer.ts
+++ src/normalizers/teams.normalizer.ts
@@ -11,8 +11,6 @@ export const normalizeTeamsPayload = (
     return {
         '@type': 'MessageCard',
         '@context': 'https://schema.org/extensions',
-        // Microsoft has deprecated color
-        // https://github.com/MicrosoftDocs/msteams-docs/issues/10062#issuecomment-1862178211
         themeColor: color,
         summary: title,
         title,
