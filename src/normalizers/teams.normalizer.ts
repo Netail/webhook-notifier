b336eb04955218e5d07e0bfad0323ce1c1f92072
Maikel <maikel.van.dort@gmail.com>
2024-08-26 16:07:02 +0200

    feat: new Teams adaptive cards

--- src/normalizers/teams.normalizer.ts
+++ src/normalizers/teams.normalizer.ts
@@ -9,26 +9,41 @@ export const normalizeTeamsPayload = (
     buttons: Button[]
 ): TeamsPayload => {
     return {
-        '@type': 'MessageCard',
-        '@context': 'https://schema.org/extensions',
-        themeColor: color,
-        summary: title,
-        title,
-        text,
-        sections: [
+        type: 'message',
+        attachments: [
             {
-                facts: fields,
+                contentType: 'application/vnd.microsoft.card.adaptive',
+                contentUrl: null,
+                content: {
+                    $schema:
+                        'http://adaptivecards.io/schemas/adaptive-card.json',
+                    type: 'AdaptiveCard',
+                    version: '1.2',
+                    body: [
+                        {
+                            type: 'TextBlock',
+                            text: title,
+                            size: 'large',
+                        },
+                        {
+                            type: 'TextBlock',
+                            text: text,
+                        },
+                        {
+                            type: 'FactSet',
+                            facts: fields.map(({ name, value }) => ({
+                                title: name,
+                                value,
+                            })),
+                        },
+                    ],
+                    actions: buttons.map(({ label, url }) => ({
+                        type: 'Action.OpenUrl',
+                        title: label,
+                        url: url,
+                    })),
+                },
             },
         ],
-        potentialAction: buttons.map((button) => ({
-            '@type': 'OpenUri',
-            name: button.label,
-            targets: [
-                {
-                    os: 'default',
-                    uri: button.url,
-                },
-            ],
-        })),
     };
 };
